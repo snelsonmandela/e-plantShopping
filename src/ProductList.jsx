5c76f56781dee241ad54eb36706b3a88b6941bd9
snelsonmandela <snelsonmandela26@gmail.com>
2025-06-17 18:58:42 +0530

    initial commit

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -312,7 +312,7 @@ function ProductList({ onHomeClick }) {
               />
               <div className="product-title">{plant.name}</div>
               <div className="product-description">{plant.description}</div>
-              <div className="product-cost">${plant.cost}</div>
+              <div className="product-cost">{plant.cost}</div>
               <button
                 className="product-button"
                 disabled={isInCart}
